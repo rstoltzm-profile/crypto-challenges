bcf16e0c56229159514c95ba4f932b8ce195928f
rstoltzm-profile <rstoltzm@gmail.com>
2025-08-04 11:37:14 -0700

    challenge 2 test

---
+++ set1/challenge2_test.go
@@ -0,0 +1,14 @@
+package set1
+
+import "testing"
+
+func TestProduceXOR(t *testing.T) {
+	input1 := "1c0111001f010100061a024b53535009181c"
+	input2 := "686974207468652062756c6c277320657965"
+	want := "746865206b696420646f6e277420706c6179"
+	got := ProduceXOR(input1, input2)
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
