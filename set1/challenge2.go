c8a2da30bce963ffe60ae943498e07e7979ec584
rstoltzm-profile <rstoltzm@gmail.com>
2025-08-04 12:08:11 -0700

    added another test and XOR function

--- set1/challenge2.go
+++ set1/challenge2.go
@@ -1,13 +1,34 @@
 package set1
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+const errorLength = "byte slices must have equal length"
 
 func ProduceXOR(input1 string, input2 string) (string, error) {
+	// decode the strings to bytes
 	d1 := decodeString(input1)
-	d2 := decodeString(input1)
+	d2 := decodeString(input2)
+
+	// validate length
 	if len(d1) != len(d2) {
-		return "", fmt.Errorf("byte slices must have equal length")
+		return "", fmt.Errorf(errorLength)
 	}
 
-	return "", nil
+	// create a result byte array
+	result := make([]byte, len(d1))
+
+	// loop through each element
+	for i := range d1 {
+		// bitwise XOR operation
+		// A ^ B = C and C ^ B = A (you can reverse the operation)
+		// specifically B can represent a key
+		result[i] = d1[i] ^ d2[i]
+	}
+
+	// encode it back to a string to return
+	encString := hex.EncodeToString(result)
+	return encString, nil
 }
--- set1/challenge2_test.go
+++ set1/challenge2_test.go
@@ -19,3 +19,17 @@ func TestProduceXOR(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestProduceXORBadSize(t *testing.T) {
+	input1 := "1c0111001f010100061a024b53535009181c"
+	input2 := "686974207468652062756c6c2773206579"
+	want := errorLength
+	_, got := ProduceXOR(input1, input2)
+	if got == nil {
+		fmt.Println("Expected error")
+	}
+
+	if got.Error() != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
